f1b1f4080a40a95e508069a05ab94a8670c57161
nikblago <159848859+oryx3422@users.noreply.github.com>
2025-11-24 18:01:07 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
+
 import torch
 print("CUDA доступна:", torch.cuda.is_available())
 print("GPU:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "Нет")
-=======
-import torch
-print("CUDA доступна:", torch.cuda.is_available())
-print("GPU:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "Нет")
->>>>>>> 813530fc33dacf6215c3545bfc88e45692c96b0a
