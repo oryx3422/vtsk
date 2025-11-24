ee4b15f8b7975d678553668e3fb4ce542be82035
nikblago <159848859+oryx3422@users.noreply.github.com>
2025-11-24 18:00:50 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,30 +1,7 @@
-<<<<<<< HEAD
 from roboflow import Roboflow
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-api_key = os.getenv("ROBOFLOW_API_KEY")
-if not api_key:
-    raise ValueError("API key not found!")
-
-rf = Roboflow(api_key=api_key)
+rf = Roboflow(api_key="#")   # Необходимо изменать на ваш roboflow Private API Key с сайта roboflow.com
 project = rf.workspace("classroom-lzi7f").project("emotion-57ymy")
 version = project.version(2)
 dataset = version.download("yolov12")
-=======
-from roboflow import Roboflow
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-api_key = os.getenv("ROBOFLOW_API_KEY")
-if not api_key:
-    raise ValueError("API key not found!")
-
-rf = Roboflow(api_key=api_key)
-project = rf.workspace("classroom-lzi7f").project("emotion-57ymy")
-version = project.version(2)
-dataset = version.download("yolov12")
->>>>>>> 813530fc33dacf6215c3545bfc88e45692c96b0a
                 
